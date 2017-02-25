ee87e77cab0fc5b272d0037bad8091b767cf2a31
unknown <estuam@SALAF401-15.UAM.LOCAL>
2017-02-25 14:15:49 -0500

    Se crea el cotrolador y las vista de la hoja de vida de ervin

--- app/Http/Controllers/hojasdevidaController.php
+++ app/Http/Controllers/hojasdevidaController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class hojasdevidaController extends Controller
 {
-    //
+   public function HdvErvin()
+    {
+       // echo "Hola mundo";
+        return view('hojadevida/ervin');
+    }
+
+    public function Hdvleonardo()
+    {
+          return view('hojadevida/leonardo');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hojadevida/ervin','hojasdevidaController@HdvErvin');
